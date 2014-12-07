76901d581b9e934b2e673cbb0b4b3cca23138a51
robertdfrench <frenchrd@ornl.gov>
2014-12-07 14:20:06 -0500

    Primitive list declaration

--- labamp.rb
+++ labamp.rb
@@ -2,15 +2,20 @@ module Labamp
   class Program
     attr_reader :c_source_code
     attr_reader :name
+    attr_accessor :headers
     attr_accessor :structs
 
     def compile
-      @c_source_code = "#include <stdio.h>\n\nint main(int argc, char** argv) {\n\tprintf(\"Hello!\\n\");\n\treturn 0;\n}"
+      @c_lines = []
+      @headers.each {|h| @c_lines << "#include <#{h}>"}
+      @structs.each {|s| @c_lines << s.to_s}
+      @c_source_code = @c_lines.join("\n")
     end
 
     def initialize(name)
       @name = name.to_s
       @structs = []
+      @headers = []
     end
 
     def double
@@ -33,6 +38,7 @@ module Labamp
 
   def program(name, &program_block)
     p = Program.new(name)
+    p.headers << "stdio.h"
     p.instance_eval &program_block
     p.compile
     puts p.c_source_code
@@ -54,36 +60,47 @@ module Labamp
 
     def to_s
       sections = []
-      sections << "typedef struct #{name} {\n\t" 
-      fields_hash.each_pair do |fieldtype,fieldname|
-        "\t#{fieldtype} #{fieldname};\n"
+      sections << "typedef struct #{name} {" 
+      fields_hash.each_pair do |fieldname,fieldtype|
+        sections << "\t#{fieldtype.declaration_template.gsub(/VARNAME/,fieldname.to_s)};"
       end
       sections << "} #{name};"
+      sections.join("\n")
     end
   end
 
   class Type
     attr_reader :name
-    
+    attr_accessor :declaration_template
+
     def initialize(name)
       @name = name
+      @declaration_template = "name VARNAME"
     end
 
     def self.by_name(name)
-      @@type_dictionary[name] = Type.new(name) if @@type_dictionary[name].nil?
+      if @@type_dictionary[name].nil?
+        log "Deriving new type #{name}"
+        @@type_dictionary[name] = Type.new(name)
+      end
       @@type_dictionary[name]
     end
 
     def self.list_of(type, length)
-      derived_name = "list(#{type},#{length})"
-      by_name(derived_name)
+      derived_name = "list(#{type.to_s},#{length})"
+      list_type = by_name(derived_name)
+      list_type.declaration_template = "#{type.to_s} VARNAME[#{length}]"
+      list_type
     end
 
     @@type_dictionary = {}
+
+    def to_s
+      name
+    end
   end
 
-  class ComposableType 
-
-
+  def log(msg)
+    puts "[LaBaMP] #{msg}"
   end
 end
